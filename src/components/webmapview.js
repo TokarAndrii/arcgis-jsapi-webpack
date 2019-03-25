cf157c25de4bf9857d2ed31ed99e1f494d1d7b3a
tokarandrii <tokar.andrii@gmail.com>
2019-03-25 16:10:48 +0200

    initial commit

--- src/components/webmapview.js
+++ src/components/webmapview.js
@@ -1,23 +1,97 @@
-import SceneView from "esri/views/SceneView";
+import MapView from "esri/views/MapView";
+import ScaleBar from "esri/widgets/ScaleBar";
+import Locate from "esri/widgets/Locate";
+import Fullscreen from "esri/widgets/Fullscreen";
+import Search from "esri/widgets/Search";
+import Compass from "esri/widgets/Compass";
+import Home from "esri/widgets/Home";
+import CoordinateConversion from "esri/widgets/CoordinateConversion";
+import Graphic from "esri/Graphic";
+import BasemapGallery from "esri/widgets/BasemapGallery";
+import Expand from "esri/widgets/Expand";
 import React from "react";
 
 
 export class WebMapComponent extends React.Component {
-  componentDidMount() {
-    const view = new SceneView({
-      map: this.props.webmap,
-      container: this.mapDiv
-    });
-    this.props.onload(view);
-  }
+    componentDidMount() {
+        const view = new MapView({
+            map: this.props.webmap,
+            container: this.mapDiv
+        });
 
-  render() {
-    return (
-      <div className="webmap"
-        ref={
-          element => this.mapDiv = element
-        }>
-      </div>
-    );
-  }
+        this.props.onload(view);
+
+
+        view.ui.add(new Fullscreen({
+            view: view,
+            element: this.mapDiv
+        }), "top-right");
+        //this.props.onload(view);
+
+        var ccWidget = new CoordinateConversion({
+            view: view
+        });
+        view.ui.add(ccWidget, "bottom-left");
+
+
+        var scalebar = new ScaleBar({
+            view: view
+        });
+        view.ui.add(scalebar, "bottom-left");
+        var locateWidget = new Locate({
+            view: view,   // Attaches the Locate button to the view
+            graphic: new Graphic({
+                symbol: {type: "simple-marker"}  // overwrites the default symbol used for the
+                // graphic placed at the location of the user when found
+            })
+        });
+
+        view.ui.add(locateWidget, "bottom-right");
+        var searchWidget = new Search({
+            view: view
+        });
+// Adds the search widget below other elements in
+// the top left corner of the view
+        view.ui.add(searchWidget, {
+            position: "top-right",
+            index: 2
+        });
+
+        var basemapGallery = new BasemapGallery({
+            view: view,
+        });
+
+        var bgExpand = new Expand({
+            view: view,
+            content: basemapGallery
+        });
+
+        view.ui.add(bgExpand, "top-right");
+
+
+
+        var compass = new Compass({
+            view: view
+        });
+
+        // adds the compass to the top left corner of the MapView
+        view.ui.add(compass, "top-left");
+
+        var homeBtn = new Home({
+            view: view
+        });
+
+        // Add the home button to the top left corner of the view
+        view.ui.add(homeBtn, "top-left");
+    }
+
+    render() {
+        return (
+            <div className="webmap"
+                 ref={
+                     element => this.mapDiv = element
+                 }>
+            </div>
+        );
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,54 +1,64 @@
 import "./config";
 
 import FeatureLayer from "esri/layers/FeatureLayer";
+import Extent from "esri/geometry/Extent";
 import WebMap from "esri/WebMap";
 
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Header } from "./components/header";
-import { WebMapComponent } from "./components/webmapview";
+import {Header} from "./components/header";
+import {WebMapComponent} from "./components/webmapview";
 
 import "./css/main.scss";
 
 const addDOMNode = () => {
-  const appNode = document.createElement("div");
-  appNode.id = "app";
-  document.body.appendChild(appNode);
-  return appNode;
+    const appNode = document.createElement("div");
+    appNode.id = "app";
+    document.body.appendChild(appNode);
+    return appNode;
 }
 
 const onComponentLoad = (view) => {
-  featureLayer.when(() => {
-    view.goTo({ target: featureLayer.fullExtent });
-  });
+    view.center = [-112, 38];  // Sets the center point of the view at a specified lon/lat
+    view.zoom = 5;  // Sets the zoom LOD to 13
+// Set the extent on the view
+    view.extent = new Extent({
+        xmin: -9177882,
+        ymin: 4246761,
+        xmax: -9176720,
+        ymax: 4247967,
+        spatialReference: {
+            wkid: 102100
+        }
+    });
+    view.scale = 24000;
 };
 
-const featureLayer = new FeatureLayer({
-  id: "states",
-  portalItem: {
-    id: "b234a118ab6b4c91908a1cf677941702"
-  },
-  outFields: ["NAME", "STATE_NAME", "VACANT", "HSE_UNITS"],
-  title: "U.S. counties"
-});
+/*const featureLayer = new FeatureLayer({
+    id: "states",
+    portalItem: {
+        id: "b234a118ab6b4c91908a1cf677941702"
+    },
+    outFields: ["NAME", "STATE_NAME", "VACANT", "HSE_UNITS"],
+    title: "U.S. counties"
+});*/
 
 const webmap = new WebMap({
-  portalItem: {
-    id: "3ff64504498c4e9581a7a754412b6a9e"
-  },
-  layers: [featureLayer]
+    portalItem: {
+        id: "15fa6c0f93704219a09f1c527616afdb"
+    },
 });
 
 /**
  * React portion of application
  */
 ReactDOM.render(
-  <div className="main">
-    <Header appName="Webpack App"/>
-    <WebMapComponent
-      webmap={webmap}
-      onload={onComponentLoad} />
-  </div>,
-  addDOMNode()
+    <div className="main">
+        <Header appName="Webpack App"/>
+        <WebMapComponent
+            webmap={webmap}
+            onload={onComponentLoad}/>
+    </div>,
+    addDOMNode()
 );
